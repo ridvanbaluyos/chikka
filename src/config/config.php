77fe25e58c40dbf98576918f96e008e9ef35f455
ridvanbaluyos <ridvan@baluyos.net>
2014-10-05 03:57:55 +0800

    Added config.php

---
+++ src/config/config.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+    /**
+     * Chikka URL via HTTP
+     * URL: https://post.chikka.com/smsapi/request
+     */
+    'url' => '',
+
+    /**
+     * Chikka Client ID
+     *
+     */
+    'client_id' => '',
+
+
+    /**
+     * Chikka Secret Key
+     *
+     */
+    'secret_key' => '',
+
+    /**
+     * Chikka Short Code (29290...)
+     *
+     */
+    'short_code' => ''
+);
